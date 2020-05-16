545e66eb6f54c534514b53c1d77dd1e2a7efd590
Ildar107 <ildar107wk@gmail.com>
2020-05-16 21:45:57 +0300

    fix: change data url

--- SpeakIt/src/components/wordItem/WordItem.jsx
+++ SpeakIt/src/components/wordItem/WordItem.jsx
@@ -11,7 +11,7 @@ const WordItem = (props) => {
             <path fill="currentColor" d="M15.788 13.007a3 3 0 110 5.985c.571 3.312 2.064 5.675 3.815 5.675 2.244 0 4.064-3.88 4.064-8.667 0-4.786-1.82-8.667-4.064-8.667-1.751 0-3.244 2.363-3.815 5.674zM19 26c-3.314 0-12-4.144-12-10S15.686 6 19 6s6 4.477 6 10-2.686 10-6 10z" fillRule="evenodd" />
           </svg>
         </span>
-        <audio src={`https://raw.githubusercontent.com/irinainina/rslang-data/master/data/${props.audio.replace('files/', '')}`} crossOrigin="anonymous">
+        <audio src={`https://raw.githubusercontent.com/Ildar107/rslang-data/master/data/${props.audio.replace('files/', '')}`} crossOrigin="anonymous">
         </audio>
         <p className="word"> {props.word}</p>
         <p className="transcription"> {props.transcription}</p>
